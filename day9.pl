35613959527b23c8e5f1046cd88c707b16d4c518
JeffBobbo <james.delph@hotmail.co.uk>
2015-12-10 16:49:31 +0000

    day9

---
+++ day9.pl
@@ -0,0 +1,49 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+
+use Sys::MemInfo qw(freemem);
+use Data::Dumper;
+
+use List::Permutor;
+
+open(my $fh, '<', 'day9.txt') or die "Failed to read input file: $!\n";
+my @lines = <$fh>;
+close($fh);
+
+my $graph = {};
+
+foreach (@lines)
+{
+  chomp();
+  my ($from, $to, $dist) = (/^([a-zA-Z]+) to ([a-zA-Z]+) = ([0-9]+)$/);
+
+  $graph->{$from}{$to} = $dist;
+  $graph->{$to}{$from} = $dist; # we can go both ways
+}
+
+my $p = List::Permutor->new(keys(%{$graph}));
+my @short;
+my $s;
+my @long;
+my $l;
+while (my @set = $p->next())
+{
+  my $cost = 0;
+  for (my $i = 0; $i < $#set; $i++)
+  {
+    $cost += $graph->{$set[$i]}{$set[$i+1]};
+  }
+  if (!defined $s || $cost < $s)
+  {
+    @short = @set;
+    $s = $cost;
+  }
+  if (!defined $l || $cost > $l)
+  {
+    @long = @set;
+    $l = $cost;
+  }
+}
+print "Shortest route is @short, covering $s distance\n";
+print "Longest route is @long, covering $l distance\n";
